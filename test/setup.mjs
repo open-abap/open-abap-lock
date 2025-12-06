ec2f2723138ab0a6c7c23899d1cc5a19b5a5ed8b
Lars HVam <larshp@hotmail.com>
2025-12-06 13:01:55 +0100

    fix

--- test/setup.mjs
+++ test/setup.mjs
@@ -1,7 +1,13 @@
 import {PostgresDatabaseClient} from "@abaplint/database-pg";
 
-export async function setup(abap, schemas, insert) {
-  abap.context.databaseConnections["DEFAULT"] = new PostgresDatabaseClient();
+export async function preFunction(abap, schemas, insert) {
+  abap.context.databaseConnections["DEFAULT"] = new PostgresDatabaseClient({
+      user: "postgres",
+      host: "localhost",
+      database: "postgres",
+      password: "postgres",
+      port: 5432,
+    });
   await abap.context.databaseConnections["DEFAULT"].connect();
   await abap.context.databaseConnections["DEFAULT"].execute(schemas.sqlite);
   await abap.context.databaseConnections["DEFAULT"].execute(insert);
